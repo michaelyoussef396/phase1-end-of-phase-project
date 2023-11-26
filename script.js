33e3be22d480d0e045ce764243324fe335e5ffa6
michael youssef <michaelyoussef396@gmail.com>
2023-11-26 20:54:14 +1100

    Implemented movie search functionality and added api key

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const movieSearchBox = document.getElementById('movie-search-box');
 const searchList = document.getElementById('search-list');
 const resultGrid = document.getElementById('result-grid');
 
+async function loadMovies(searchTerm) {
+    const URL = `http://www.omdbapi.com/?s=${searchTerm}&apikey=857379f2`;
+    const res = await fetch(URL);
+    const data = await res.json();
+    console.log(data);
+}
