dde3e27a6e435064c929fbae2232a18b9b1b3fa7
michael youssef <michaelyoussef396@gmail.com>
2023-11-26 23:26:38 +1100

    Added event listeners for search and favorites, implemented functionality for movie search triggering via the search button, enabled movie addition to favorites, and updated search result rendering using array iteration

--- script.js
+++ script.js
@@ -28,28 +28,34 @@ function findMovies() {
         loadMovies(searchTerm);
     }
 }
-function displayMovieList(movies){
+function displayMovieList(movies) {
     searchList.innerHTML = "";
-    for(let idx = 0; idx < movies.length; idx++){
-        let movieListItem = document.createElement('div');
-        movieListItem.dataset.id = movies[idx].imdbID; // setting movie id in  data-id
+    movies.forEach(movie => {
+        const movieListItem = document.createElement('div');
+        movieListItem.dataset.id = movie.imdbID; 
         movieListItem.classList.add('search-list-item');
-        if(movies[idx].Poster != "N/A")
-            moviePoster = movies[idx].Poster;
-        else 
-            moviePoster = "https://artsmidnorthcoast.com/wp-content/uploads/2014/05/no-image-available-icon-6-300x188.png";
 
-        movieListItem.innerHTML = `
-        <div class = "search-item-thumbnail">
-            <img src = "${moviePoster}">
-        </div>
-        <div class = "search-item-info">
-            <h3>${movies[idx].Title}</h3>
-            <p>${movies[idx].Year}</p>
-        </div>
+        const thumbnail = document.createElement('div');
+        thumbnail.classList.add('search-item-thumbnail');
+        const poster = movie.Poster !== "N/A" ? movie.Poster : "image_not_found.png";
+        thumbnail.innerHTML = `<img src="${poster}" alt="Movie Poster">`;
+
+        const movieInfo = document.createElement('div');
+        movieInfo.classList.add('search-item-info');
+        movieInfo.innerHTML = `
+            <h3>${movie.Title}</h3>
+            <p>${movie.Year}</p>
         `;
+
+        const addToFavoritesButton = document.createElement('button');
+        addToFavoritesButton.textContent = 'Add to Favorites';
+        addToFavoritesButton.classList.add('add-to-favorites');
+        movieListItem.appendChild(thumbnail);
+        movieListItem.appendChild(movieInfo);
+        movieListItem.appendChild(addToFavoritesButton);
+
         searchList.appendChild(movieListItem);
-    }
+    });
     loadMovieDetails();
 }
 
@@ -89,6 +95,9 @@ function displayMovieDetails(details){
         <p class = "language"><b>Language:</b> ${details.Language}</p>
         <p class = "awards"><b><i class = "fas fa-award"></i></b> ${details.Awards}</p>
     </div>
+    <div class="like-icon" onclick="toggleLike(this)">
+        <i class="far fa-heart"></i>
+    </div>
     `;
 }
 
@@ -102,7 +111,13 @@ window.addEventListener('click', (event) => {
 function toggleDarkMode() {
     const body = document.body;
     const isDarkMode = body.classList.toggle('dark-mode');
-    saveDarkModePreference(isDarkMode); // Save the dark mode preference in local storage
+
+    const bottomSection = document.querySelectorAll('.favorite-controls, .favorite-movies, .liked-posters');
+    bottomSection.forEach(element => {
+        element.classList.toggle('dark-mode', isDarkMode);
+    });
+
+    saveDarkModePreference(isDarkMode);
 }
 
 function saveDarkModePreference(isDarkMode) {
@@ -157,3 +172,109 @@ function sortMovies() {
         searchList.appendChild(movie);
     });
 }
+
+function addToFavorites(movie) {
+    const clonedMovie = movie.cloneNode(true);
+    clonedMovie.removeEventListener('click', null); 
+    favoriteMoviesContainer.appendChild(clonedMovie);
+}
+
+function loadMovieDetails() {
+    const searchListMovies = searchList.querySelectorAll('.search-list-item');
+    searchListMovies.forEach(movie => {
+        movie.addEventListener('click', async () => {
+            searchList.classList.add('hide-search-list');
+            movieSearchBox.value = '';
+            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=fc1fef96`);
+            const movieDetails = await result.json();
+            displayMovieDetails(movieDetails);
+
+            addToFavorites(movie);
+        });
+    });
+}
+
+function sortFavorites() {
+    const favoriteMovies = Array.from(favoriteMoviesContainer.querySelectorAll('.search-list-item'));
+    const sortedFavorites = favoriteMovies.sort((a, b) => {
+        const movieA = a.querySelector('.search-item-info h3').textContent;
+        const movieB = b.querySelector('.search-item-info h3').textContent;
+        return movieA.localeCompare(movieB);
+    });
+
+    favoriteMoviesContainer.innerHTML = '';
+    sortedFavorites.forEach(movie => {
+        favoriteMoviesContainer.appendChild(movie);
+    });
+}
+
+const likedMovies = [];
+
+function toggleLike(likeIcon) {
+    const movieDetails = likeIcon.parentElement;
+    const movieId = movieDetails.dataset.id;
+
+    if (!likedMovies.includes(movieId)) {
+        likedMovies.push(movieId);
+        likeIcon.innerHTML = '<i class="fas fa-heart"></i>'; 
+    } else {
+        const index = likedMovies.indexOf(movieId);
+        likedMovies.splice(index, 1);
+        likeIcon.innerHTML = '<i class="far fa-heart"></i>'; 
+    }
+
+}
+
+function toggleLike(likeIcon) {
+    const movieDetails = likeIcon.parentElement;
+    const movieId = movieDetails.dataset.id;
+    const moviePoster = movieDetails.querySelector('.movie-poster img').src; // Get the movie poster URL
+
+    if (!likedMovies.includes(movieId)) {
+        likedMovies.push(movieId);
+        likeIcon.innerHTML = '<i class="fas fa-heart"></i>'; // Change icon to filled heart for liked movie
+        displayLikedPoster(moviePoster); // Display the liked movie poster at the bottom
+    } else {
+        const index = likedMovies.indexOf(movieId);
+        likedMovies.splice(index, 1);
+        likeIcon.innerHTML = '<i class="far fa-heart"></i>'; // Change icon to outline heart for unliked movie
+        removeLikedPoster(moviePoster); // Remove the unliked movie poster from the bottom
+    }
+
+    // You can store likedMovies array in local storage or send it to server for further processing
+}
+
+function displayLikedPoster(posterUrl) {
+    const likedPosters = document.getElementById('liked-posters');
+    const posterImage = document.createElement('img');
+    posterImage.src = posterUrl;
+    posterImage.classList.add('liked-poster-item');
+    likedPosters.appendChild(posterImage);
+}
+
+function removeLikedPoster(posterUrl) {
+    const likedPosters = document.getElementById('liked-posters');
+    const posters = likedPosters.querySelectorAll('.liked-poster-item');
+    posters.forEach(poster => {
+        if (poster.src === posterUrl) {
+            poster.remove();
+        }
+    });
+}
+
+const searchButton = document.getElementById('search-button'); // Assuming the search button has an ID of 'search-button'
+searchButton.addEventListener('click', () => {
+    const searchTerm = movieSearchBox.value.trim();
+    if (searchTerm.length > 0) {
+        loadMovies(searchTerm);
+    }
+});
+
+
+function addToFavorites(movie) {
+    const addToFavoritesButtons = document.querySelectorAll('.add-to-favorites');
+    addToFavoritesButtons.forEach(button => {
+        button.addEventListener('click', () => {
+        });
+    });
+};
