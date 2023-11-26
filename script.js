7cc1d4d1f731ab07ecacc92c6ec16fbb2b3d3e6a
michael youssef <michaelyoussef396@gmail.com>
2023-11-26 20:50:17 +1100

    Added element selectors

---
+++ script.js
@@ -0,0 +1,4 @@
+const movieSearchBox = document.getElementById('movie-search-box');
+const searchList = document.getElementById('search-list');
+const resultGrid = document.getElementById('result-grid');
+
