b28e37fe42a889847f5e51843d9d66f9de9865b6
michael youssef <michaelyoussef396@gmail.com>
2023-11-26 21:32:53 +1100

    Implemented dynamic movie list generation from fetched data

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ async function loadMovies(searchTerm) {
     const res = await fetch(URL);
     const data = await res.json();
     // console.log(data.Search);
-    if(data.Response == "True") displayMovieList(data.Search);
+    if (data.Response == "True") {
+        displayMovieList(data.Search);
+    }
 }
 
-function findMovies(){
-    let SearchTerm = (movieSearchBox.value).trim();
-    if(searchTerm.length > 0){
+function findMovies() {
+    let searchTerm = (movieSearchBox.value).trim(); 
+    if (searchTerm.length > 0) {
         searchList.classList.remove('hide-search-list');
         loadMovies(searchTerm);
     } else {
@@ -24,6 +26,27 @@ function displayMovieList(movies){
     searchList.innerHTML = "";
     for(let idx = 0; idx < movies.length; idx++){
         let movieListItem = document.createElement('div');
-        console.log(movieListItem);
+        movieListItem.dataset.id = movies[idx].imdbID; // setting movie id in  data-id
+        movieListItem.classList.add('search-list-item');
+        if(movies[idx].Poster != "N/A")
+            moviePoster = movies[idx].Poster;
+        else 
+            moviePoster = "https://artsmidnorthcoast.com/wp-content/uploads/2014/05/no-image-available-icon-6-300x188.png";
+
+        movieListItem.innerHTML = `
+        <div class = "search-item-thumbnail">
+            <img src = "${moviePoster}">
+        </div>
+        <div class = "search-item-info">
+            <h3>${movies[idx].Title}</h3>
+            <p>${movies[idx].Year}</p>
+        </div>
+        `;
+        searchList.appendChild(movieListItem);
     }
+    loadMovieDetails();
 }
+
+
+
+
